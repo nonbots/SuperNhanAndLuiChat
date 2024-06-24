6200843615ad93c8791298f1ba92c775f2cb96b0
nonbots <nhantdbui@gmail.com>
2024-06-24 13:22:24 -0400

    Feature: instantiate many asteroids

--- app.c
+++ app.c
@@ -4,12 +4,13 @@
 const int screenWidth = 500;
 const int screenHeight = 500;
 const int minSpeed = 1;
-const int maxSpeed = 20;
+const int maxSpeed = 10;
 const int minRadius = 5;
 const int maxRadius = 20;
 const int minXPosition = 0;
 const int maxXPosition = screenWidth;
-//asteriod struct
+const int asteroidsLength = 10;
+//asteroid struct
 typedef struct asteroid {
   int radius;
   int xPosition;
@@ -17,15 +18,20 @@ typedef struct asteroid {
   int currentSpeed;
 } asteroid_t;
 
+void initAsteroid(asteroid_t* ast) {
 
-void drawAsteroid(asteroid_t* ast) {
+  ast->radius = GetRandomValue(minRadius, maxRadius);
+  ast->xPosition = GetRandomValue(minXPosition, maxXPosition);
+  ast->yPosition = 0; 
+  ast->currentSpeed = GetRandomValue(minSpeed,maxSpeed);
+}
+void updateAsteroid(asteroid_t* ast) {
   if (ast->yPosition >= screenHeight + ast->radius) {
     ast->yPosition = 0 - ast->radius;
     ast->xPosition = GetRandomValue(minXPosition, maxXPosition);
     ast->radius = GetRandomValue(minRadius, maxRadius);
     ast->currentSpeed = GetRandomValue(minSpeed, maxSpeed);
   }
-
   ast->yPosition = ast->yPosition + ast->currentSpeed;
   DrawCircle(ast->xPosition, ast->yPosition, ast->radius, (Color){102, 153, 204, 255});
 }
@@ -34,18 +40,20 @@ int main(void) {
   InitWindow(screenWidth, screenHeight, "raylib [shapes] example - basic shapes drawing");
   SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 
-  asteroid_t a = { 
-    GetRandomValue(minRadius, maxRadius), 
-    GetRandomValue(minXPosition, maxXPosition),
-    0, 
-    GetRandomValue(minSpeed,maxSpeed) 
-  };
-  // Main game loop
+  //create an array of asteroid instances 
+  asteroid_t asteroids[asteroidsLength];
+  //loop through asteroids to init each asteroid
+  for (int i = 0; i < asteroidsLength; i++) {
+    initAsteroid(&asteroids[i]);
+  }
+ // Main game loop
   while (!WindowShouldClose())    // Detect window close button or ESC key
   {
     BeginDrawing();
     ClearBackground((Color){0,255,0,255});
-    drawAsteroid(&a);
+    for (int j = 0; j < asteroidsLength; j++) {
+      updateAsteroid(&asteroids[j]);
+    }
     EndDrawing();
   }
 
